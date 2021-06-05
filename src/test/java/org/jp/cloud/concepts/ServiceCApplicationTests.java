3d4628914330a73eab285002f312be94d7aed44d
jaspreet <jaspreetjhass91@gmail.com>
2021-06-06 02:46:49 +0530

    initial commit

---
+++ src/test/java/org/jp/cloud/concepts/ServiceCApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jp.cloud.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
